674c419ba8333d0fb591ffc13993b5e7b6e8eeb9
Ayon Bhattacharya <ayon.ux@gmail.com>
2025-12-27 08:32:44 -0600

    beat the race condition! wooooo git push

--- 5_race/race.c
+++ 5_race/race.c
@@ -14,3 +14,37 @@ Observation: It won't be 2,000,000. It will be random and lower. This is a race 
 The Fix: Introduce pthread_mutex_t. Lock before incrementing, unlock after. Verify the result is now exactly 2,000,000.
 
 */
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <string.h>     
+#include <unistd.h>     
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <pthread.h>
+
+int counter = 0;
+pthread_mutex_t counter_lock = PTHREAD_MUTEX_INITIALIZER;
+
+void * counter_worker() {
+
+    for (size_t i=0;i < 1000000;i++){
+        pthread_mutex_lock(&counter_lock);
+        counter++;
+        pthread_mutex_unlock(&counter_lock);
+    }
+    
+    
+}
+
+int main() {
+    pthread_t thread_id1;
+    pthread_t thread_id2;
+    
+    pthread_create(&thread_id1, NULL, counter_worker, NULL);
+    pthread_create(&thread_id2, NULL, counter_worker, NULL);
+    pthread_join(thread_id1, NULL);
+    pthread_join(thread_id2, NULL);
+    printf("Counter: %d\n", counter);
+
+}
