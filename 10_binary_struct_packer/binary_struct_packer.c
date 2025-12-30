f9cf59839b1b55777f9a8d1b4fd7e6b986477d18
root <ayon.b@protonmail.com>
2025-12-30 17:47:51 +0000

    binary struct packer

--- 10_binary_struct_packer/binary_struct_packer.c
+++ 10_binary_struct_packer/binary_struct_packer.c
@@ -14,3 +14,66 @@ Write a serialize function that packs these into a generic char buffer using hto
 Write a deserialize function that unpacks them back using ntohl.
 
 */
+
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+struct sample {
+    int number;
+    short small_number;
+    char letter;
+};
+
+void serialize(struct sample *item, char *buffer) {
+    int ordered_number = htonl(item -> number);
+    memcpy(buffer, &ordered_number, sizeof(ordered_number));
+    buffer += sizeof(int);
+
+    short ordered_short = htons(item -> small_number);
+    memcpy(buffer, &ordered_short, sizeof(short));
+    buffer += sizeof(short);
+
+    *buffer = item -> letter;
+}
+
+struct sample deserialize(char * buffer) {
+    struct sample item;  
+    int ordered_int;
+    short ordered_short;
+    
+    memcpy(&ordered_int, buffer, sizeof(int));
+    item.number = ntohl(ordered_int);
+    buffer += sizeof(int);
+
+    memcpy(&ordered_short, buffer, sizeof(short));
+    item.small_number = ntohs(ordered_short);
+    buffer += sizeof(short);
+
+    item.letter = *buffer;
+
+    return item;
+}
+
+int main() {
+    struct sample item;
+    item.number = 12;
+    item.small_number = 10;
+    item.letter = 'A';
+
+    char buffer[sizeof(item)]; 
+    
+    serialize(&item, buffer);
+
+    printf("Serialized Buffer (Hex): ");
+    for (int i = 0; i < sizeof(int) + sizeof(short) + sizeof(char); i++) {
+        printf("%02x ", (unsigned char)buffer[i]);
+    }
+    printf("\n");
+
+    struct sample deserialized = deserialize(buffer);
+    
+    printf("Deserialized: %d, %d, %c\n", deserialized.number, deserialized.small_number, deserialized.letter);
+
+    return 0;
+}
