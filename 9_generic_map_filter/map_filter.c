d3fc6840e196981b73b1b3975f75ef74813b9289
root <ayon.b@protonmail.com>
2025-12-30 04:09:32 +0000

    map filter done

--- 9_generic_map_filter/map_filter.c
+++ 9_generic_map_filter/map_filter.c
@@ -14,4 +14,22 @@ It should take an array of any type (ints, structs) and apply func to every elem
 Challenge: Pointer arithmetic on void* is illegal. You have to cast to char* and add item_size manually to iterate.
 
 */
+#include <stdio.h>
 
+ void map(void *array, int length, size_t item_size, void (*func)(void*)) {
+    for (int i = 0; i < length; i++) {
+        char* ptr = (char*)array + (item_size * i);
+        func(ptr);
+    }
+ }
+
+ void print_stuff(void* ptr) {
+    int integer = *(int*)ptr;
+    printf("Printing %d.\n", integer);
+ }
+
+ int main() {
+    int integers[] = {1,2,3,4};
+    int length = sizeof(integers) /sizeof(integers[0]);
+    map(integers, 4, sizeof(int), print_stuff);
+ }
